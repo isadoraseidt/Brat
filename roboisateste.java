672f7bcc954d53e2bf0aac7cd2804b132f49cbfc
isadoraseidt <isadora.se02@aluno.edu.ifsc.br>
2024-12-05 20:06:48 -0300

    DEixando só o código das cores

--- roboisateste.java
+++ roboisateste.java
@@ -17,43 +17,7 @@ public class Teste1Isa extends Robot {
 		setBulletColor(Color.blue);
 		setScanColor(Color.pink);
 		
-		while (true) {
-			
-			setAhead(40000);
-			movingForward = true;
-			setTurnRight(90);
-			waitFor(new TurnCompleteCondition(this));
-			setTurnLeft(180);
-			waitFor(new TurnCompleteCondition(this));
-			setTurnRight(180);
-			waitFor(new TurnCompleteCondition(this))
 	}
 }
 
-public void onHitWall(HitWallEvent e) {
 
-		reverseDirection();
-	}
-
-
-	public void reverseDirection() {
-		if (movingForward) {
-			setBack(40000);
-			movingForward = false;
-		} else {
-			setAhead(40000);
-			movingForward = true;
-		}
-	}
-
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(1);
-	}
-
-	public void onHitRobot(HitRobotEvent e) {
-		
-		if (e.isMyFault()) {
-			reverseDirection();
-		}
-	}
-}
