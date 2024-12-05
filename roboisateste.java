e3a967326c4d36ef0f1085c378ff3df262fbcb98
isadoraseidt <isadora.se02@aluno.edu.ifsc.br>
2024-12-05 18:12:50 -0300

    Refazendo os testes da cores

--- roboisateste.java
+++ roboisateste.java
@@ -13,42 +13,6 @@ public class Teste1Isa extends AdvancedRobot {
 		
 		setColors(Color.pink, Color.green, Color.black);
 		
-		while (true) {
-			
-			setAhead(40000);
-			movingForward = true;
-			setTurnRight(90);
-			waitFor(new TurnCompleteCondition(this));
-			setTurnLeft(180);
-			waitFor(new TurnCompleteCondition(this));
-			setTurnRight(180);
-			waitFor(new TurnCompleteCondition(this));
-		}
-	}
-
-	public void onHitWall(HitWallEvent e) {
-
-		reverseDirection();
-	}
-
-
-	public void reverseDirection() {
-		if (movingForward) {
-			setBack(40000);
-			movingForward = false;
-		} else {
-			setAhead(40000);
-			movingForward = true;
-		}
-	}
-
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(1);
-	}
-
-	public void onHitRobot(HitRobotEvent e) {
-		if (e.isMyFault()) {
-			reverseDirection();
-		}
 	}
 }
+	
