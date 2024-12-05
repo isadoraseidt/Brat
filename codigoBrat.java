c939e19f5fb0da42c5861e7318ef292fd4b6a7b9
Haydee <haydee.m@aluno.ifsc.edu.br>
2024-12-05 20:10:45 -0300

    Teste de cores

---
+++ codigoBrat.java
@@ -0,0 +1,15 @@
+package sample;
+
+
+import robocode.JuniorRobot;
+public class Brat extends Robot {
+public void run() {
+		setColors(pink, blue, yellow);
+
+		while (true) {
+			ahead(100);
+			turnGunRight(360); 
+			back(100); 
+			turnGunRight(360); 
+		}
+	}
