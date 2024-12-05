a5b7ed32b6acf1be8005021d735665fa0f75f864
isadoraseidt <isadora.se02@aluno.edu.ifsc.br>
2024-12-05 19:38:59 -0300

    Teste de um Robo Crazy

--- roboisateste.java
+++ roboisateste.java
@@ -6,6 +6,7 @@ import java.awt.Colors;
 
 
 public class Teste1Isa extends Robot {
+	boolean movingForward;
 
 	
 	public void run() {
@@ -15,6 +16,44 @@ public class Teste1Isa extends Robot {
 		setRadarColor(Color.green);
 		setBulletColor(Color.blue);
 		setScanColor(Color.pink);
+		
+		while (true) {
+			
+			setAhead(40000);
+			movingForward = true;
+			setTurnRight(90);
+			waitFor(new TurnCompleteCondition(this));
+			setTurnLeft(180);
+			waitFor(new TurnCompleteCondition(this));
+			setTurnRight(180);
+			waitFor(new TurnCompleteCondition(this))
 	}
 }
-	
+
+public void onHitWall(HitWallEvent e) {
+
+		reverseDirection();
+	}
+
+
+	public void reverseDirection() {
+		if (movingForward) {
+			setBack(40000);
+			movingForward = false;
+		} else {
+			setAhead(40000);
+			movingForward = true;
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
+
+	public void onHitRobot(HitRobotEvent e) {
+		
+		if (e.isMyFault()) {
+			reverseDirection();
+		}
+	}
+}
