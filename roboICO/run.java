f0a9f1dfe20bf441326fca51b212f0d0234a005c
Haydee <haydee.m@aluno.ifsc.edu.br>
2024-12-17 19:58:50 -0300

    Primeiro commit branch run

---
+++ roboICO/run.java
@@ -0,0 +1,15 @@
+mport robocode.*;
+import java.awt.Color;
+
+
+public class Main extends Robot {
+
+   
+    public void run() {
+
+        while (true) {
+            ahead(50);
+            turnRight(45);
+        }
+    }
+}
