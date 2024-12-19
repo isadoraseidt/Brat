fb3920ba8458b0fa8ed4271d762bb1a47669f86d
IsaacKozuchovski <isaackozuchovski@gmail.com>
2024-12-19 19:24:16 -0300

    Fazendo merge das funcionalidades restantes no arquivo run

--- roboICO/run.java
+++ roboICO/run.java
@@ -19,3 +19,22 @@ setScanColor(Color.magenta);
         }
     }
 }
+public void onScannedRobot(ScannedRobotEvent e) {
+        String robotank = e.getName();
+        double distancia = e.getDistance();
+        System.out.println(robotank + " distância: " + distancia);
+        
+        if (distancia < 135) {
+            fire(3); 
+        } else {
+            fire(1);
+        }
+    }
+public void onHitWall(HitWallEvent e) {
+        back(50);
+        turnRight(90);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+ahead(10);
+}
